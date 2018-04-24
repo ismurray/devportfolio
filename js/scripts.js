0182c3cecb90078c5a83399284ce72f5b2b99597
ismurray <ians.murray@yahoo.com>
2018-04-24 12:37:53 -0400

    Speed up Navbar scroll animations
    Make the navbar link scrolling animation not so maddeningly slow.

--- js/scripts.js
+++ js/scripts.js
@@ -27,7 +27,8 @@
 
         $('html, body').animate({
             scrollTop: scrollDistance + 'px'
-        }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
+        // }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1.5);
+        }, 500);
 
         // Hide the menu once clicked if mobile
         if ($('header').hasClass('active')) {
